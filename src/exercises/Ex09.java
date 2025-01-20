a8b923fc83435976576000a5c2715792f9516ca7
daigo <takumi.daigo@rakus-partners.co.jp>
2025-01-20 14:45:43 +0900

    update Ex09

--- src/exercises/Ex09.java
+++ src/exercises/Ex09.java
@@ -1,7 +1,22 @@
 package exercises;
 
+import java.time.LocalDateTime;
+
 public class Ex09 {
 
-    
+    public static void main(String[] args) {
+        
+        LocalDateTime localDateTime = LocalDateTime.of(2020, 1, 1, 0, 0, 0);
+        // System.out.println("日付と時間：" + localDateTime);
+
+        localDateTime = localDateTime.plusYears(1);
+        localDateTime = localDateTime.plusMonths(2);
+        localDateTime = localDateTime.plusDays(3);
+        localDateTime = localDateTime.plusHours(4);
+        localDateTime = localDateTime.plusMinutes(5);
+        localDateTime = localDateTime.plusSeconds(6);
+
+        System.out.println(localDateTime);
+    }
     
 }
