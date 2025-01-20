b852bfbcbe4a28c31b094c9c34c3aa450e5b984a
daigo <takumi.daigo@rakus-partners.co.jp>
2025-01-20 10:21:01 +0900

    first git commit

---
+++ src/exercises/Ex01.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class Ex01 {
+    
+    public static void main(String[] args) {
+        
+        String message = "I have a dream today";
+
+        int charNum = message.length();
+
+        System.out.println(charNum + " 文字です");
+    }
+}
